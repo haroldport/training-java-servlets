869b41e20c1bdc84e01fa33f6db63f08f39da86c
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-16 14:54:58 -0500

    ejemplo con radio button

---
+++ src/com/arquitecturajava/ServletFormulario004.java
@@ -0,0 +1,19 @@
+package com.arquitecturajava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletFormulario004 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.println(request.getParameter("curso"));
+	}
+
+}
