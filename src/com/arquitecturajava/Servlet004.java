f5e86994a8744ef689736ae313f0a9e1c260571f
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-17 16:43:48 -0500

    ejemplo de Servlet con parametros de inicializacion

--- src/com/arquitecturajava/Servlet004.java
+++ src/com/arquitecturajava/Servlet004.java
@@ -3,6 +3,7 @@ package com.arquitecturajava;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,11 @@ public class Servlet004 extends HttpServlet {
 	//variable compartida por todas las peticiones
 	private int peticiones;
 	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		peticiones = Integer.parseInt(config.getInitParameter("inicio"));
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();
 		pw.println(peticiones);
